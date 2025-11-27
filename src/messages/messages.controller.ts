315e170bd7a5f487c9c958367ae29d5cd0f4f947
Oleg <luzeninoleg2012@gmail.com>
2025-11-27 21:29:12 +0200

    implemented repositositories, services

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,27 +1,37 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 @Controller('/messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    //Don't do this in real app, use dependency injection
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
   listMessages() {
     return {
-      message: 'Api is good',
+      messages: this.messagesService.findAll(),
       status: 200,
     };
   }
 
   @Post()
-  createMessage(@Body() body: any) {
-    console.log(body);
+  createMessage(@Body() body: CreateMessageDto) {
+    return {
+      messages: this.messagesService.create(body.content),
+      status: 201,
+    };
   }
 
   @Get('/:id')
   getMessagesById(@Param() id: string) {
-    console.log(id);
     return {
-      message: 'Got the param',
+      messages: this.messagesService.findOne(id),
       status: 200,
-      param: id,
     };
   }
 }
